253de89cccc4b5b80e8e3cd3c1abaef086dea6db
Alisa <ally8252@gmail.com>
2019-07-22 02:06:42 +0600

    Add adaptive

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ $('.slider').slick(
 		arrows: false,
 		dots: true
 	});
+
+const anchors = document.querySelectorAll('nav a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href");
+
+    if (anchor.getAttribute("href") === "#") {
+      document.querySelector("body").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    } else {
+      document.querySelector("" + blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
+  });
+}
