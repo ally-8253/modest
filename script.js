93d333440721f68b6c3a073fbeb6ddb7a50fc424
Alisa <ally8252@gmail.com>
2019-07-23 19:05:26 +0600

    Add burger-menu

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-$('.slider').slick(
-	{
-		autoplay: true,
-		arrows: false,
-		dots: true
-	});
-
-const anchors = document.querySelectorAll('nav a[href*="#"]');
-
-for (let anchor of anchors) {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-
-    const blockID = anchor.getAttribute("href");
-
-    if (anchor.getAttribute("href") === "#") {
-      document.querySelector("body").scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-      });
-    } else {
-      document.querySelector("" + blockID).scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-      });
-    }
-  });
-}
+$('.menu-hidden-btn').on('click', function(e) {
+  e.preventDefault;
+  $(this).toggleClass('menu-hidden-btn-active');
+  $('.menu-nav').toggleClass('menu-nav-active');
+});
